0336b7dc93315f31be3d0854996549bd7907eb54
romie151 <romiezelaya@ac.c4q.nyc>
2017-11-16 14:37:03 -0500

    adding the modules exercises

---
+++ math-module.js
@@ -0,0 +1,17 @@
+const sum = (num1, num2) => num1 + num2;
+
+
+sum(2, 3)
+
+module.exports {
+    sum: sum
+}
+
+const sum = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+module.exports = {
+  sum: sum,
+  multiply: multiply
+}
